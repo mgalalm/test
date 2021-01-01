6805565996da87eb093bf8994a3a22bf1edd38e0
mgalalm <squareno2@gmail.com>
2021-01-01 22:07:13 +0000

    Update client.js

--- client.js
+++ client.js
@@ -5,7 +5,7 @@ const app = express()
 //defining the root endpoint
 app.get('/', (req, res) => {
      res.send('Hello World!')
-
+}
 //specifying the listening port
 app.listen(8081, ()=>{
     console.log('Listening on port 8081')
